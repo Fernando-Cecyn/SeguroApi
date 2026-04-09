a79d2925919fad0d259a56495d3c73fb2025e5d1
Fernando-Cecyn <FernandoHenriqueCecyn@gmail.com>
2026-04-09 13:20:39 -0300

    feat: Ajuste Cors Local Host

--- SeguroApiRestfull/Program.cs
+++ SeguroApiRestfull/Program.cs
@@ -5,7 +5,7 @@ using Seguro.Infrastructure.Data;
 using Seguro.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
-
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 builder.Services.AddControllers();
 
 builder.Services.AddDbContext<SeguroDbContext>(opt =>
@@ -17,8 +17,18 @@ builder.Services.AddScoped<SeguroService>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+builder.Services.AddCors(options =>
+{   
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("http://localhost:4200/").AllowAnyMethod().AllowAnyOrigin().AllowAnyHeader();
+                      });
+});
 
+var app = builder.Build();
+app.UseCors(MyAllowSpecificOrigins);
+app.UseCors();
 app.UseSwagger();
 app.UseSwaggerUI();
 
